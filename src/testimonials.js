c6a268f4b0975e2a0901086aeea1afe46d5b7756
Kevin Ngo <me@ngokevin.com>
2019-07-04 04:44:54 -0700

    add more testimonial

---
+++ src/testimonials.js
@@ -0,0 +1,75 @@
+module.exports = [
+  {
+    name: 'John B.',
+    location: 'Kentucky',
+    why: '"He is the only candidate trying to think of how to handle the automation future that\'s coming down the line. We have already seen the affects from China doing those jobs, but now as automation takes even more jobs that can\'t be outsourced, we will have MASSIVE unemployment and we need to plan NOW on how to handle that. That\'s why I will vote for him."',
+    candidate: 'Gary Johnson',
+    username: '/u/Collective82'
+  },
+  {
+    name: 'Polissa C.',
+    location: 'Florence, AL',
+    why: '"I voted Trump last time to avoid what I saw was a bigger evil. I am not voting Trump again. He is even worse than he came off before he got elected. He is in it for himself and to puff up his ego. Nothing has changed for our family since Trump entered office other than price of everything except gas has gone up it seems. I am disabled, and my husband works in food. My income is not enough to survive on much less thrive. And my husband\'s income at $10p/h at 33-/+hrs a week and must keep open availability to get hours isn\'t working. He loves his job. But it is a chain store and the number one complaint from restaurants is consistency of product. Automated kitchens are coming fast. The fancier places will survive but it\'s the chain stores like my husband\'s who will automate to save cost and operate with skeleton crews. What happens to us then. Yang is the only one saying what I\'ve been saying for the last 7 years. The future is now, and we can\'t afford to wait and make a plan tomorrow."',
+    candidate: 'Donald Trump',
+    username: '@CampbellPolissa'
+  },
+  {
+    name: 'Chandler W.',
+    location: 'Bellevue, WA',
+    why: '"He\'s the only candidate talking about the real issues that are affecting and will be affecting people\'s daily lives - automation and AI displacing human jobs. In order to effectively tackle climate change and other issues, we need to address the mindset of scarcity that is affecting so many Americans (like the 78% that can\'t afford an extra $500 monthly expense). I\'m a conservative but above all else, I\'m a patriot that cares about where my country is headed if the Democrats and Republicans don\'t get their acts together."',
+    username: '@chandlerbwils0n',
+    candidate: 'Gary Johnson'
+  },
+  {
+    name: 'Brandon B.',
+    location: 'Dallas, TX',
+    why: '"I have always considered myself a reasonable conservative. Someone who rarely voted straight down party lines and would always look for opportunities to see compromises. Andrew Yang represents that to me. My biggest problem with most democratic candidates is that I don\'t feel as though they recognize the problems that everyday Americans face. Andrew Yang, I believe, is the only candidate who not only recognizes the problems Americans face, but offers solutions based in today\'s America not solutions from decades past. I will always vote for someone who recognizes the issues over someone who doesn\'t, or manufactures new issues."',
+    candidate: 'Donald Trump',
+    username: '@brandon_b12'
+  },
+  {
+    name: 'Ryan W.',
+    location: 'USA',
+    why: '"Raised conservative...pretty much my whole family is. I voted for Trump as a lesser of two evils also. I was never his biggest fan, but he won the Republican primary and Bernie lost the Democratic primary so I felt I had 2 sucky choices. I want Yang to win because Yang represents to me actually having a real choice in this election."',
+    candidate: 'Donald Trump'
+  },
+  {
+    name: 'Nicholas K.',
+    location: 'Ohio',
+    why: '"I watched Andrew Yang on Joe Rogan\'s Podcast. They way he talked with reason and logic on issues. I loved how well versed he was on economic issues and how they will effect Americans.  I am a Conservative that believes in Climate Change but I think the Democrats have been very narrow on their solutions. Yang being one of the only candidates R or D that supports Nuclear energy makes me think he is forward thinking and isn\'t just pandering to a base. He believes in practical solutions for problems that face the world and the United States. This only means he has my vote and my first ever political donation."',
+    candidate: 'Gary Johnson'
+  },
+  {
+    name: 'Sam M.',
+    location: 'New York',
+    why: '"Heard him on Ben Shapiro\'s Sunday special, and was surprised by his character, intelligence, common-sense, humane philosophy, torrent of fresh ideas, Everything! Even while I don\'t support some of his policies, he will still be the best president America has had in decades."',
+    candidate: 'Abstained'
+  },
+  {
+    name: 'Fred R.',
+    location: 'Mariscopa, AZ',
+    why: '"I met with him and he rode in my truck."',
+    username: '@felon_fred',
+    image: '/assets/firstTestimonial.jpg',
+    candidate: 'Donald Trump'
+  },
+  {
+    name: 'Sarah M.',
+    location: 'New York',
+    why: '"His ideas are the best way forward. Period."',
+    candidate: 'Donald Trump'
+  },
+  {
+    name: 'Scott H.',
+    location: 'Latham, NY',
+    why: '"Support all of his forward thinking, bipartisan ideas."',
+    candidate: 'Donald Trump'
+  },
+  {
+    name: 'Kelton K.',
+    location: 'Phoenix, AZ',
+    why: '"Yes!"',
+    username: '@tasteslikewall',
+    candidate: 'Donald Trump'
+  },
+];
