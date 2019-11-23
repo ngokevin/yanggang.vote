98da4470e94677b598fd4adb5a938533fada37b1
Ubuntu <ubuntu@ip-172-31-20-41.us-west-2.compute.internal>
2019-11-23 10:42:05 +0000

    cron

---
+++ src/cron.js
@@ -0,0 +1,7 @@
+const cron = require('node-cron');
+const execSync = require('child_process').execSync();
+
+cron.schedule('*/10 * * * *', () => {
+  execSync('npm run count', {cwd: 'redditBot'});
+  execSync('npm run deploy');
+});
